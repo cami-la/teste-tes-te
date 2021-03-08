1a6b7c72419e20659617bfeb92621c8a990ca50c
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-03-08 05:41:23 -0300

    inital commit

---
+++ src/br/com/dio/collection/Exercicio01.java
@@ -0,0 +1,7 @@
+package br.com.dio.collection;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("Primeiro exercício!");
+    }
+}
---
+++ src/br/com/dio/collection/Main.java
@@ -0,0 +1,8 @@
+package br.com.dio.collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
